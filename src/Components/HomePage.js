2d84e036277c49d9fd78db00619046e2e4a53831
aredhead1370 <71857488+aredhead1370@users.noreply.github.com>
2020-12-12 11:46:34 -0500

    Nothing

---
+++ src/Components/HomePage.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const HomePage = () => {
+    <p>
+    We are Touchless, providng hand free ways of conducting business to ensure 
+    </p>
+};
+
+export default HomePage;
